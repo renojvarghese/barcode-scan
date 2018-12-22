6c45f57049bb4869a7e7beb4fa608745205bb378
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-22 18:28:50 -0500

    adding ScanBarcode component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ScanBarcode msg="This·is·your·Scan·Barcode" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ScanBarcode from "./components/ScanBarcode.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    ScanBarcode
   }
 };
 </script>
---
+++ src/components/ScanBarcode.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="scan">
+    <h1>{{ msg }}</h1>
+     <picture-input 
+      ref="pictureInput"
+      width="600" 
+      height="600" 
+      margin="16" 
+      accept="image/jpeg,image/png" 
+      size="10" 
+      />
+  </div>
+</template>
+
+<script>
+import PictureInput from "vue-picture-input";
+
+export default {
+  name: "ScanBarcode",
+  props: {
+    msg: String
+  },
+  components: {
+    PictureInput
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
