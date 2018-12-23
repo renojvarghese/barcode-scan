5556303c69f66888f6d7c28efc80f59050435016
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-23 18:46:10 -0500

    integrating quagga and getting barcode from image

--- src/components/ScanBarcode.vue
+++ src/components/ScanBarcode.vue
@@ -7,18 +7,72 @@
       width="450"
       height="320"
       :plain="true"
+      :crop="false"
+      :hideChangeButton="true"
+      @change="onChange"
       />
+    </div>
+    <div v-if="error" class="error">
+      {{error}}
+    </div>
+    <div v-if="barcode" class="barcode">
+      {{barcode}}
     </div>
   </div>
 </template>
 
 <script>
 import PictureInput from "vue-picture-input";
-
+import Quagga from "quagga";
+import Vue from "vue";
 export default {
   name: "ScanBarcode",
+  data: () => ({
+    barcode: null,
+    image: null,
+    error: null
+  }),
   components: {
     PictureInput
+  },
+  methods: {
+    onChange: function(image) {
+      this.image = image;
+      let quaggaConfig = {
+        inputStream: {
+          size: 1000,
+          singleChannel: false
+        },
+        locator: {
+          patchSize: "large",
+          halfSample: false
+        },
+        decoder: {
+          readers: [
+              "upc_reader",
+              "code_128_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "ean_8_reader",
+              "ean_reader",
+              "upc_e_reader",
+              "codabar_reader"
+          ]
+        },
+        locate: true,
+        src: image
+      }
+      Quagga.decodeSingle(quaggaConfig, result => {
+        if (result.codeResult) {
+          this.barcode = result.codeResult.code
+          this.error = null;
+        }
+        else {
+          this.barcode = null;
+          this.error = "barcode not found";
+        }
+      })
+    }
   }
 };
 </script>
