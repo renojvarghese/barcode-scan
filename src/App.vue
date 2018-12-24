58da17b93cf841ef1433e359407b48ab075c928b
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-23 19:54:56 -0500

    adding styles for barcode and error containers

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,18 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400i,600');
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Source Sans Pro", Helvetica, Arial, sans-serif;
+  font-weight: 600;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   max-width: 786px;
   margin: 0 auto;
   box-shadow: 0 0 10px 1px rgba(0,0,0,0.05);
+}
+body {
+  margin: 0;
 }
 </style>
--- src/components/ScanBarcode.vue
+++ src/components/ScanBarcode.vue
@@ -15,8 +15,9 @@
     <div v-if="error" class="error">
       {{error}}
     </div>
-    <div v-if="barcode" class="barcode">
-      {{barcode}}
+    <div v-if="barcode" class="barcode-container">
+      <h2 class="barcode-title">Barcode</h2>
+      <div class="barcode">{{barcode}}</div>
     </div>
   </div>
 </template>
@@ -24,7 +25,7 @@
 <script>
 import PictureInput from "vue-picture-input";
 import Quagga from "quagga";
-import Vue from "vue";
+
 export default {
   name: "ScanBarcode",
   data: () => ({
@@ -81,6 +82,7 @@ export default {
 <style scoped lang="scss">
 .scan {
   min-height:110vh;
+  padding:15pt;
 }
 .pict-btn {
   margin:0 auto;
@@ -98,20 +100,50 @@ export default {
     pointer-events: none;
   }
   &:before {
-    background: url("../assets/camera-icon.svg") no-repeat center center green;
+    background: url("../assets/camera-icon.svg") no-repeat center center;
+    background-size: 100% 100%;
+    width: 80%;
+    height: 80%;
+    top:10%;
+    left: 10%;
   }
   &:after {
     z-index: 10002;
-    background: black;
-    opacity:0;
+    background: green;
+    opacity:1;
   }
   &.hasImage {
     &:before {
-      background: url("../assets/camera-icon.svg") no-repeat center center;
+      background: url("../assets/camera-icon.svg") no-repeat cover center center;
     }
     &:after {
-      opacity: .25;;
+      background: black;
+      opacity: .25;
     }
   }
+}
+.error {
+  color:red;
+  font-size: 16pt;
+  font-weight: 400;
+  font-style: italic;
+  overflow-wrap: break-word;
+  margin-top: 20px;
+}
+.barcode-container {
+  text-align: left;
+  margin: 0 auto;
+  max-width: 450px;
+  margin-top:20px;
+  overflow-wrap: break-word;
+}
+.barcode-title {
+  font-size: 24pt;
+}
+.barcode {
+  color:green;
+  font-size: 24pt;
+  letter-spacing: 4pt;
+  text-align: center;
 }
 </style>
